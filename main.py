73e428949aca24322a45d130c07afb72c546fd28
sunjian <sunjxan@gmail.com>
2021-11-12 00:55:03 +0800

    mnist

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torchvision as tv
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from tensorboardX import SummaryWriter
 
 import os, time
 
@@ -38,21 +39,23 @@ class Net(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2), padding_mode='zeros')
         self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2), padding_mode='zeros')
-        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(in_features=20*7*7, out_features=50)
         self.fc2 = nn.Linear(in_features=50, out_features=10)
+        self.bn = nn.BatchNorm2d(20)
         self._init_parameters()
 
     def forward(self, x):
         # (64, 1, 28, 28)
-        x = self.pool(F.relu(self.conv1(x)))
+        x = F.max_pool2d(self.conv1(x), 2)
+        x = F.relu(x)
         # (64, 10, 14, 14)
-        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = self.bn(x)
         # (64, 20, 7, 7)
         x = nn.Flatten()(x)
         # (64, 980)
         x = F.relu(self.fc1(x))
-        # (64, 50)
         x = F.dropout(x, p=dropout_rate, training=self.training)
         # (64, 50)
         x = self.fc2(x)
@@ -69,6 +72,8 @@ class Net(nn.Module):
                 nn.init.zeros_(layer.bias)
 
 
+# 可视化，tensorboard --logdir=runs
+writer = SummaryWriter()
 # 设备
 # 执行前设置环境变量 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" python3 mnist.py
 cuda_available = torch.cuda.is_available()
@@ -80,6 +85,7 @@ else:
 device = torch.device("cuda:0" if cuda_available else "cpu")
 # 模型
 model = Net()
+writer.add_graph(model, (torch.zeros(1, 1, 28, 28), ))
 if cuda_available and device_count > 1:
     model = nn.DataParallel(model, device_ids=list(range(device_count)), output_device=0)
 model = model.to(device)
@@ -89,10 +95,10 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 
 
+global_step = 0
 steps_per_epoch = len(train_loader)
-train_images = 0
-train_counter = []
-train_losses = []
+input_images = 0
+correct_images = 0
 
 for epoch_index in range(epochs):
     epoch_begin = time.time()
@@ -111,26 +117,29 @@ for epoch_index in range(epochs):
 
         step_end = time.time()
 
-        train_images += batch_size_train
-        train_counter.append(train_images)
-        train_losses.append(loss.item())
+        correct_images += (torch.argmax(outputs, -1) == labels).sum().item()
+        input_images += batch_size_train
 
-        print('Epoch {}/{}  Step {}/{}  Time: {:.0f}ms  Loss: {:.6f}'.format(epoch_index + 1, 2 * epochs, batch_index + 1, steps_per_epoch, (step_end - step_begin) * 1e3, loss.item()))
+        global_step += 1
+        writer.add_scalar('train/loss', loss.item(), global_step)
+        writer.add_scalar('train/accuracy', correct_images / input_images, global_step)
+
+        print('Epoch {}/{}  Step {}/{}  Time: {:.0f}ms  Loss: {:.6f}  Accuracy: {:.1f}%'.format(epoch_index + 1, 2 * epochs, batch_index + 1, steps_per_epoch, (step_end - step_begin) * 1e3, loss.item(), 1e2 * correct_images / input_images))
 
     epoch_end = time.time()
-    print('Epoch {}/{}  Time: {:.1f}s  Avg Loss: {:.6f}'.format(epoch_index + 1, 2 * epochs, epoch_end - epoch_begin, sum(train_losses[-steps_per_epoch:]) / steps_per_epoch))
+    print('[Epoch {}/{}]  Time: {:.1f}s'.format(epoch_index + 1, 2 * epochs, epoch_end - epoch_begin))
 
-    
-torch.save(model.state_dict(), 'parameters.pt')
-torch.save(optimizer.state_dict(), 'optimizer.pt')
+
+torch.save(model.state_dict(), 'parameters.pkl')
+torch.save(optimizer.state_dict(), 'optimizer.pkl')
 
 model2 = Net()
 if cuda_available and device_count > 1:
     model2 = nn.DataParallel(model2, device_ids=list(range(device_count)), output_device=0)
-model2.load_state_dict(torch.load('parameters.pt'))
+model2.load_state_dict(torch.load('parameters.pkl'))
 model2 = model2.to(device)
 optimizer2 = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
-optimizer2.load_state_dict(torch.load('optimizer.pt'))
+optimizer2.load_state_dict(torch.load('optimizer.pkl'))
 
 
 for epoch_index in range(epochs, 2 * epochs):
@@ -150,38 +159,34 @@ for epoch_index in range(epochs, 2 * epochs):
 
         step_end = time.time()
 
-        train_images += batch_size_train
-        train_counter.append(train_images)
-        train_losses.append(loss.item())
+        correct_images += (torch.argmax(outputs, -1) == labels).sum().item()
+        input_images += batch_size_train
 
-        print('Epoch {}/{}  Step {}/{}  Time: {:.0f}ms  Loss: {:.6f}'.format(epoch_index + 1, 2 * epochs, batch_index + 1, steps_per_epoch, (step_end - step_begin) * 1e3, loss.item()))
+        global_step += 1
+        writer.add_scalar('train/loss', loss.item(), global_step)
+        writer.add_scalar('train/accuracy', correct_images / input_images, global_step)
+
+        print('Epoch {}/{}  Step {}/{}  Time: {:.0f}ms  Loss: {:.6f}  Accuracy: {:.1f}%'.format(epoch_index + 1, 2 * epochs, batch_index + 1, steps_per_epoch, (step_end - step_begin) * 1e3, loss.item(), 1e2 * correct_images / input_images))
 
     epoch_end = time.time()
-    print('Epoch {}/{}  Time: {:.1f}s  Avg Loss: {:.6f}'.format(epoch_index + 1, 2 * epochs, epoch_end - epoch_begin, sum(train_losses[-steps_per_epoch:]) / steps_per_epoch))
+    print('[Epoch {}/{}]  Time: {:.1f}s'.format(epoch_index + 1, 2 * epochs, epoch_end - epoch_begin))
 
 
-import matplotlib.pyplot as plt
-fig = plt.figure()
-plt.plot(train_counter, train_losses, color='blue')
-plt.legend(['Train Loss'], loc='upper right')
-plt.xlabel('images count')
-plt.ylabel('loss')
-plt.savefig('losses.png')
+torch.save(model2, 'model.pkl')
 
-
-torch.save(model2, 'model.pt')
-
-model3 = torch.load('model.pt')
+model3 = torch.load('model.pkl')
 if cuda_available and device_count > 1:
     model3 = nn.DataParallel(model3, device_ids=list(range(device_count)), output_device=0)
 model3 = model3.to(device)
 
 
-steps_test = len(test_loader)
-test_images = 0
+global_step = 0
+steps_total = len(test_loader)
+input_images = 0
 correct_images = 0
-test_losses_sum = 0
+
 with torch.no_grad():
+    test_begin = time.time()
     for batch_index, (images, labels) in enumerate(test_loader):
         step_begin = time.time()
 
@@ -194,8 +199,14 @@ with torch.no_grad():
         step_end = time.time()
 
         correct_images += (torch.argmax(outputs, -1) == labels).sum().item()
-        test_images += batch_size_test
-        test_losses_sum += loss.item()
+        input_images += batch_size_test
 
-        print('Step {}/{}  Time: {:.0f}ms  Avg Loss: {:.6f}'.format(batch_index + 1, steps_test, (step_end - step_begin) * 1e3, test_losses_sum / steps_test))
-print('Test  Avg Loss: {:.6f}  Accuracy: {}/{} ({:.1f}%)'.format(test_losses_sum / steps_test, correct_images, test_images, 1e2 * correct_images / test_images))
+        global_step += 1
+        writer.add_scalar('test/loss', loss.item(), global_step)
+        writer.add_scalar('test/accuracy', correct_images / input_images, global_step)
+
+        print('Step {}/{}  Time: {:.0f}ms  Loss: {:.6f}  Accuracy: {:.1f}%'.format(batch_index + 1, steps_total, (step_end - step_begin) * 1e3, loss.item(), 1e2 * correct_images / input_images))
+    test_end = time.time()
+print('[Test]  Time: {:.1f}s  Accuracy: {}/{} ({:.1f}%)'.format(test_end - test_begin, correct_images, input_images, 1e2 * correct_images / input_images))
+
+writer.close()
