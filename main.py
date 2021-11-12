e9e4509e7353c27903f37da5f0b68f816c805aa1
Jian Sun <sunjxan@gmail.com>
2021-11-12 17:42:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -95,47 +95,51 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 
 
+def fit(model, optimizer, epochs, initial_epoch=1):
+    global global_step
+    
+    steps_per_epoch = len(train_loader)
+    total_train_images = len(train_set)
+
+    for epoch_index in range(initial_epoch, initial_epoch + epochs):
+        epoch_loss_sum = 0
+        epoch_correct_images = 0
+        epoch_begin = time.time()
+
+        for batch_index, (images, labels) in enumerate(train_loader):
+            step_input_images = images.shape[0]
+            
+            step_begin = time.time()
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            step_end = time.time()
+            step_period = round((step_end - step_begin) * 1e3)
+
+            global_step += 1
+            step_loss = loss.item()
+            epoch_loss_sum += step_loss * step_input_images
+            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
+            epoch_correct_images += step_correct_images
+            writer.add_scalar('train/loss', step_loss, global_step)
+            writer.add_scalar('train/accuracy', step_correct_images / step_input_images, global_step)
+
+            print('Epoch {}/{}  Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index, initial_epoch + epochs - 1, batch_index + 1, steps_per_epoch, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
+
+        epoch_end = time.time()
+        epoch_period = round((epoch_end - epoch_begin) * 1e3)
+        print('[Epoch {}/{}]  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index, initial_epoch + epochs - 1, int(epoch_period / 1e3), epoch_period % 1e3, epoch_loss_sum / total_train_images, epoch_correct_images, total_train_images, 1e2 * epoch_correct_images / total_train_images))
+
+
 global_step = 0
-steps_per_epoch = len(train_loader)
-total_train_images = len(train_set)
-
-for epoch_index in range(epochs):
-
-    epoch_loss_sum = 0
-    epoch_correct_images = 0
-    epoch_begin = time.time()
-
-    for batch_index, (images, labels) in enumerate(train_loader):
-        step_input_images = images.shape[0]
-        
-        step_begin = time.time()
-
-        images = images.to(device)
-        labels = labels.to(device)
-
-        optimizer.zero_grad()
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        step_end = time.time()
-        step_period = round((step_end - step_begin) * 1e3)
-
-        global_step += 1
-        step_loss = loss.item()
-        epoch_loss_sum += step_loss * step_input_images
-        step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
-        epoch_correct_images += step_correct_images
-        writer.add_scalar('train/loss', step_loss, global_step)
-        writer.add_scalar('train/accuracy', step_correct_images / step_input_images, global_step)
-
-        print('Epoch {}/{}  Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index + 1, 2 * epochs, batch_index + 1, steps_per_epoch, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
-
-    epoch_end = time.time()
-    epoch_period = round((epoch_end - epoch_begin) * 1e3)
-    print('[Epoch {}/{}]  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index + 1, 2 * epochs, int(epoch_period / 1e3), epoch_period % 1e3, epoch_loss_sum / total_train_images, epoch_correct_images, total_train_images, 1e2 * epoch_correct_images / total_train_images))
-
+fit(model, optimizer, epochs, 1)
 
 torch.save(model.state_dict(), 'parameters.pkl')
 torch.save(optimizer.state_dict(), 'optimizer.pkl')
@@ -148,37 +152,7 @@ model2 = model2.to(device)
 optimizer2 = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 optimizer2.load_state_dict(torch.load('optimizer.pkl'))
 
-
-for epoch_index in range(epochs, 2 * epochs):
-    epoch_begin = time.time()
-
-    for batch_index, (images, labels) in enumerate(train_loader):
-        input_images = images.shape[0]
-    
-        step_begin = time.time()
-
-        images = images.to(device)
-        labels = labels.to(device)
-
-        optimizer.zero_grad()
-        outputs = model2(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer2.step()
-
-        step_end = time.time()
-        step_period = step_end - step_begin
-
-        global_step += 1
-        correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
-        writer.add_scalar('train/loss', loss.item(), global_step)
-        writer.add_scalar('train/accuracy', correct_images / input_images, global_step)
-
-        print('Epoch {}/{}  Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index + 1, 2 * epochs, batch_index + 1, steps_per_epoch, int(step_period), int(step_period * 1e3) % 1e3, loss.item(), correct_images, input_images, 1e2 * correct_images / input_images))
-
-    epoch_end = time.time()
-    epoch_period = epoch_end - epoch_begin
-    print('[Epoch {}/{}]  Time: {:.0f}s {:.0f}ms'.format(epoch_index + 1, 2 * epochs, int(epoch_period), int(epoch_period * 1e3) % 1e3))
+fit(model, optimizer2, epochs, epochs + 1)
 
 
 torch.save(model2, 'model.pkl')
@@ -189,37 +163,47 @@ if cuda_available and device_count > 1:
 model3 = model3.to(device)
 
 
-global_step = 0
-steps_total = len(test_loader)
-step_correct_images = 0
-total_correct_images = 0
-total_input_images = len(test_set)
+def evaluate(model):
+    global global_step
+    
+    steps_total = len(test_loader)
+    total_loss_sum = 0
+    total_correct_images = 0
+    total_input_images = 0
 
-with torch.no_grad():
-    test_begin = time.time()
-    for batch_index, (images, labels) in enumerate(test_loader):
-        step_input_images = images.shape[0]
+    with torch.no_grad():
+        test_begin = time.time()
+
+        for batch_index, (images, labels) in enumerate(test_loader):
+            step_input_images = images.shape[0]
+            total_input_images += step_input_images
+            
+            step_begin = time.time()
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            step_end = time.time()
+            step_period = round((step_end - step_begin) * 1e3)
+
+            global_step += 1
+            step_loss = loss.item()
+            total_loss_sum += step_loss * step_input_images
+            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
+            total_correct_images += step_correct_images
+            writer.add_scalars('test/loss', {'current': step_loss, 'average': total_loss_sum / total_input_images}, global_step)
+            writer.add_scalars('test/accuracy', {'current': step_correct_images / step_input_images, 'average': total_correct_images / total_input_images}, global_step)
+
+            print('Test  Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(batch_index + 1, steps_total, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
         
-        step_begin = time.time()
+        test_end = time.time()
+        test_period = round((test_end - test_begin) * 1e3)
+        print('[Test]  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(int(test_period / 1e3), test_period % 1e3, total_loss_sum / total_input_images, total_correct_images, total_input_images, 1e2 * total_correct_images / total_input_images))
 
-        images = images.to(device)
-        labels = labels.to(device)
-
-        outputs = model3(images)
-        loss = criterion(outputs, labels)
-
-        step_end = time.time()
-        step_period = step_end - step_begin
-
-        global_step += 1
-        step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
-        total_correct_images += step_correct_images
-        writer.add_scalar('test/loss', loss.item(), global_step)
-        writer.add_scalar('test/accuracy', step_correct_images / step_input_images, global_step)
-
-        print('Test Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(batch_index + 1, steps_total, int(step_period), int(step_period * 1e3) % 1e3, loss.item(), step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
-    test_end = time.time()
-    test_period = test_end - test_begin
-print('[Test]  Time: {:.0f}s {:.0f}ms  Accuracy: {}/{} ({:.1f}%)'.format(int(test_period), int(test_period * 1e3) % 1e3, total_correct_images, total_input_images, 1e2 * total_correct_images / total_input_images))
+global_step = 0
+evaluate(model3)
 
 writer.close()
