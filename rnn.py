8d39880dbbfaf61c25187c5ef90a242dfb955c20
sunjian <sunjxan@gmail.com>
2021-11-17 01:19:39 +0800

    rnn

--- rnn.py
+++ rnn.py
@@ -5,165 +5,71 @@ import torch.optim as optim
 from tensorboardX import SummaryWriter
 
 import os, time
+import numpy as np
 
 
-epochs = 10
-batch_size_train = 60
-batch_size_test = 1000
-learning_rate = 0.01
-momentum = 0.5
-log_interval_steps = 200
+TIME_STEP = 10 # rnn 时序步长数
+INPUT_SIZE = 1 # rnn 的输入维度
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+H_SIZE = 64 # of rnn 隐藏单元个数
+EPOCHS=300 # 总共训练次数
+h_state = None # 隐藏层状态
 
 
-# 转换器，将PIL Image转换为Tensor
-transform = tv.transforms.Compose([tv.transforms.ToTensor(), torch.squeeze])
-if not os.path.isdir('./data/MNIST'):
-    # 训练集，(60000, 2, 1, 28, 28)
-    train_set = tv.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    # 测试集，(10000, 2, 1, 28, 28)
-    test_set = tv.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-else:
-    train_set = tv.datasets.MNIST(root='./data', train=True, download=False, transform=transform)
-    test_set = tv.datasets.MNIST(root='./data', train=False, download=False, transform=transform)
-# 训练数据生成器
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_train, shuffle=True)
-# 测试数据生成器
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size_test, shuffle=False)
+steps = np.linspace(0, np.pi*2, 256, dtype=np.float32)
+x_np = np.sin(steps)
+y_np = np.cos(steps)
 
 
-class Net(nn.Module):
+class RNN(nn.Module):
     def __init__(self):
-        super().__init__()
-        # 将batch维度放到最顶层
-        self.rnn = nn.RNN(28, 28, 1, batch_first=True)
-        self.fc = nn.Linear(in_features=28, out_features=10, bias=True)
-    def forward(self, x):
-        outputs, hxs = self.rnn(x)
-        # 取输出的向量outputs中的最后一个向量最为最终输出
-        x = outputs[:, -1, :]
-        x = self.fc(x)
-        return x
+        super(RNN, self).__init__()
+        self.rnn = nn.RNN(
+        input_size=INPUT_SIZE,
+        hidden_size=H_SIZE,
+        num_layers=1,
+        batch_first=True,
+        )
+        self.out = nn.Linear(H_SIZE, 1)
+    def forward(self, x, h_state):
+         # x (batch, time_step, input_size)
+         # h_state (n_layers, batch, hidden_size)
+         # r_out (batch, time_step, hidden_size)
+        r_out, h_state = self.rnn(x, h_state)
+        outs = [] # 保存所有的预测值
+        for time_step in range(r_out.size(1)): # 计算每一步长的预测值
+            outs.append(self.out(r_out[:, time_step, :]))
+        return torch.stack(outs, dim=1), h_state
+         # 也可使用以下这样的返回值
+         # r_out = r_out.view(-1, 32)
+         # outs = self.out(r_out)
+         # return outs, h_state
 
+rnn = RNN().to(DEVICE)
+optimizer = torch.optim.Adam(rnn.parameters()) # Adam优化，几乎不用调参
+criterion = nn.MSELoss() # 因为最终的结果是一个数值，所以损失函数用均方误差
 
-# 可视化 tensorboard --logdir=runs --bind_all
-writer = SummaryWriter(logdir='runs-rnn')
-# 设备
-# 执行前设置环境变量 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" python3 mnist.py
-cuda_available = torch.cuda.is_available()
-if cuda_available:
-    device_count = torch.cuda.device_count()
-    print('use {} gpu(s)'.format(device_count))
-else:
-    print('use cpu')
-# 程序中会对可见GPU重新从0编号
-device = torch.device("cuda:0" if cuda_available else "cpu")
-# 模型
-model = Net()
-writer.add_graph(model, (torch.zeros(1, 28, 28), ))
-if cuda_available and device_count > 1:
-    model = nn.DataParallel(model, device_ids=list(range(device_count)), output_device=0)
-model = model.to(device)
-# 损失函数
-criterion = nn.CrossEntropyLoss()
-# 优化器
-optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
-
-
-def fit(model, optimizer, epochs, initial_epoch=0):
-    global global_step
-
-    steps_per_epoch = len(train_loader)
-    total_train_images = len(train_set)
-
-    for epoch_index in range(initial_epoch, initial_epoch + epochs):
-        epoch_loss_sum = 0
-        epoch_correct_images = 0
-        epoch_begin = time.time()
-
-        for step_index, (images, labels) in enumerate(train_loader):
-            step_input_images = images.shape[0]
-
-            step_begin = time.time()
-
-            images = images.to(device)
-            labels = labels.to(device)
-
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            step_end = time.time()
-            step_period = round((step_end - step_begin) * 1e3)
-
-            global_step += 1
-            step_loss = loss.item()
-            epoch_loss_sum += step_loss * step_input_images
-            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
-            epoch_correct_images += step_correct_images
-            
-            if step_index and step_index % log_interval_steps == 0:
-                torch.save(model.state_dict(), 'lenet-parameters.pkl')
-                torch.save(optimizer.state_dict(), 'lenet-optimizer.pkl')
-
-                writer.add_scalar('train/loss', step_loss, global_step)
-                writer.add_scalar('train/accuracy', step_correct_images / step_input_images, global_step)
-
-                print('Epoch {}/{}  Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index + 1, initial_epoch + epochs, step_index + 1, steps_per_epoch, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
-
-        epoch_end = time.time()
-        epoch_period = round((epoch_end - epoch_begin) * 1e3)
-
-        print('[Epoch {}/{}]  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index + 1, initial_epoch + epochs, int(epoch_period / 1e3), epoch_period % 1e3, epoch_loss_sum / total_train_images, epoch_correct_images, total_train_images, 1e2 * epoch_correct_images / total_train_images))
-
-global_step = 0
-fit(model, optimizer, epochs, 0)
-
-
-def evaluate(model):
-    global global_step
-
-    steps_total = len(test_loader)
-    total_loss_sum = 0
-    total_correct_images = 0
-    total_input_images = 0
-
-    with torch.no_grad():
-        test_begin = time.time()
-
-        for step_index, (images, labels) in enumerate(test_loader):
-            step_input_images = images.shape[0]
-            total_input_images += step_input_images
-
-            step_begin = time.time()
-
-            images = images.to(device)
-            labels = labels.to(device)
-
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-
-            step_end = time.time()
-            step_period = round((step_end - step_begin) * 1e3)
-
-            global_step += 1
-            step_loss = loss.item()
-            total_loss_sum += step_loss * step_input_images
-            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
-            total_correct_images += step_correct_images
-
-            writer.add_scalars('test/loss', {'current': step_loss, 'average': total_loss_sum / total_input_images}, global_step)
-            writer.add_scalars('test/accuracy', {'current': step_correct_images / step_input_images, 'average': total_correct_images / total_input_images}, global_step)
-
-            print('Evaluate  Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(step_index + 1, steps_total, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
-
-        test_end = time.time()
-        test_period = round((test_end - test_begin) * 1e3)
-
-        print('[Evaluate]  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(int(test_period / 1e3), test_period % 1e3, total_loss_sum / total_input_images, total_correct_images, total_input_images, 1e2 * total_correct_images / total_input_images))
-
-global_step = 0
-evaluate(model)
-
-writer.close()
+rnn.train()
+plt.figure(2)
+for step in range(EPOCHS):
+    start, end = step * np.pi, (step+1)*np.pi # 一个时间周期
+    steps = np.linspace(start, end, TIME_STEP, dtype=np.float32)
+    x_np = np.sin(steps)
+    y_np = np.cos(steps)
+    x = torch.from_numpy(x_np[np.newaxis, :, np.newaxis]) # shape (batch, time_step, input_size)
+    y = torch.from_numpy(y_np[np.newaxis, :, np.newaxis])
+    x=x.to(DEVICE)
+    prediction, h_state = rnn(x, h_state) # rnn output
+    # 这一步非常重要
+    h_state = h_state.data # 重置隐藏层的状态, 切断和前一次迭代的链接
+    loss = criterion(prediction.cpu(), y)
+    # 这三行写在一起就可以
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if (step+1)%20==0: #每训练20个批次可视化一下效果，并打印一下loss
+        print("EPOCHS: {},Loss:{:4f}".format(step,loss))
+        plt.plot(steps, y_np.flatten(), 'r-')
+        plt.plot(steps, prediction.cpu().data.numpy().flatten(), 'b-')
+        plt.draw()
+        plt.pause(0.01)
