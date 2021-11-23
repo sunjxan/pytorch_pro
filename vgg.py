94da611e41d309c3fb616a27fcbb734a55ad77a1
sunjian <sunjxan@gmail.com>
2021-11-24 01:00:53 +0800

    新增网络

---
+++ vgg.py
@@ -0,0 +1,230 @@
+import torch
+import torchvision as tv
+import torch.nn as nn
+import torch.optim as optim
+from tensorboardX import SummaryWriter
+
+import os, time
+
+
+model_pkl = 'vgg.pkl'
+parameters_pkl = 'vgg-parameters.pkl'
+optimizer_pkl = 'vgg-optimizer.pkl'
+epochs = 200
+batch_size_train = 100
+batch_size_val = 1000
+learning_rate = 1e-3
+log_interval_steps = 250
+# 对于可重复的实验，设置随机种子
+torch.manual_seed(seed=1)
+
+
+# 转换器，将PIL Image转换为Tensor
+transform = tv.transforms.Compose([tv.transforms.ToTensor()])
+# 训练集（有标签），(100000, 2, 3, 64, 64)
+train_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/train', transform=transform)
+# 验证集（有标签），(10000, 2, 3, 64, 64)
+val_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/val', transform=transform)
+# 测试集（无标签），(10000, 2, 3, 64, 64)
+test_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/test', transform=transform)
+# 训练数据生成器，先随机打乱，然后按batch_size分批，样本不重不漏，最后一个batch样本数量可能不足batch_size
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_train, shuffle=True)
+# 测试数据生成器
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size_val, shuffle=False)
+
+
+cfg = [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=1000):
+        super().__init__()
+        self.features = features
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+# 可视化 tensorboard --logdir=runs-vgg --bind_all
+writer = SummaryWriter(logdir='runs-vgg')
+# 设备
+# 执行前设置环境变量 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" python3 filename.py
+cuda_available = torch.cuda.is_available()
+if cuda_available:
+    device_count = torch.cuda.device_count()
+    print('use {} gpu(s)'.format(device_count))
+else:
+    print('use cpu')
+# 程序中会对可见GPU重新从0编号
+device = torch.device("cuda:0" if cuda_available else "cpu")
+# 模型
+model = VGG(make_layers(cfg, batch_norm=True))
+if os.path.isfile(parameters_pkl):
+    model.load_state_dict(torch.load(parameters_pkl))
+else:
+    # PyTorch版本不同预训练权重地址可能不同  https://download.pytorch.org/models/vgg11_bn-6002323d.pth
+    model.load_state_dict(torch.load("/home/sunjian/.cache/torch/hub/checkpoints/vgg11_bn-6002323d.pth"))
+if cuda_available and device_count > 1:
+    model = nn.DataParallel(model, device_ids=list(range(device_count)), output_device=0)
+model = model.to(device)
+# 损失函数
+criterion = nn.CrossEntropyLoss()
+# 优化器
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+if os.path.isfile(optimizer_pkl):
+    optimizer.load_state_dict(torch.load(optimizer_pkl))
+
+
+def fit(model, optimizer, epochs, initial_epoch=1, baseline=True):
+    global global_step
+
+    # 设置model.training为True，使模型中的Dropout和BatchNorm起作用
+    model.train()
+
+    steps_per_epoch = len(train_loader)
+    total_train_images = len(train_set)
+
+    for epoch_index in range(initial_epoch, initial_epoch + epochs):
+        print('Train Epoch {}/{}'.format(epoch_index, initial_epoch + epochs - 1))
+        print('-' * 20)
+
+        epoch_loss_sum = 0
+        epoch_correct_images = 0
+        epoch_begin = time.time()
+
+        for step_index, (images, labels) in enumerate(train_loader, 1):
+            step_input_images = images.shape[0]
+
+            step_begin = time.time()
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            step_end = time.time()
+            step_period = round((step_end - step_begin) * 1e3)
+
+            global_step += 1
+            step_loss = loss.item()
+            epoch_loss_sum += step_loss * step_input_images
+            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
+            epoch_correct_images += step_correct_images
+
+            if step_index % log_interval_steps == 0:
+                torch.save(model.state_dict(), 'vgg-parameters.pkl')
+                torch.save(optimizer.state_dict(), 'vgg-optimizer.pkl')
+
+                writer.add_scalar('train/loss', step_loss, global_step)
+                writer.add_scalar('train/accuracy', step_correct_images / step_input_images, global_step)
+
+                print('Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(step_index, steps_per_epoch, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
+
+        epoch_end = time.time()
+        epoch_period = round((epoch_end - epoch_begin) * 1e3)
+
+        print('-' * 20)
+        print('Train Epoch {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index, initial_epoch + epochs - 1, int(epoch_period / 1e3), epoch_period % 1e3, epoch_loss_sum / total_train_images, epoch_correct_images, total_train_images, 1e2 * epoch_correct_images / total_train_images))
+        print()
+
+    if baseline:
+        steps_total_val = len(val_loader)
+        baseline_loss = epoch_loss_sum / total_train_images
+        baseline_accuracy = epoch_correct_images / total_train_images
+        for i in range(1, steps_total_val + 1):
+            writer.add_scalars('test/loss', {'baseline': baseline_loss}, i)
+            writer.add_scalars('test/accuracy', {'baseline': baseline_accuracy}, i)
+
+global_step = 0
+fit(model, optimizer, epochs)
+
+
+def evaluate(model):
+    global global_step
+
+    # 设置model.training为False，使模型中的Dropout和BatchNorm不起作用
+    model.eval()
+
+    steps_total = len(val_loader)
+    total_loss_sum = 0
+    total_correct_images = 0
+    total_input_images = 0
+
+    with torch.no_grad():
+        print('Eval')
+        print('-' * 20)
+
+        val_begin = time.time()
+
+        for step_index, (images, labels) in enumerate(val_loader, 1):
+            step_input_images = images.shape[0]
+            total_input_images += step_input_images
+
+            step_begin = time.time()
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            step_end = time.time()
+            step_period = round((step_end - step_begin) * 1e3)
+
+            global_step += 1
+            step_loss = loss.item()
+            total_loss_sum += step_loss * step_input_images
+            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
+            total_correct_images += step_correct_images
+
+            writer.add_scalars('validate/loss', {'current': step_loss, 'average': total_loss_sum / total_input_images}, global_step)
+            writer.add_scalars('validate/accuracy', {'current': step_correct_images / step_input_images, 'average': total_correct_images / total_input_images}, global_step)
+
+            print('Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(step_index, steps_total, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
+
+        val_end = time.time()
+        val_period = round((val_end - val_begin) * 1e3)
+
+        print('-' * 20)
+        print('Eval  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(int(val_period / 1e3), val_period % 1e3, total_loss_sum / total_input_images, total_correct_images, total_input_images, 1e2 * total_correct_images / total_input_images))
+        print()
+
+global_step = 0
+evaluate(model)
+
+
+torch.save(model, model_pkl)
+writer.add_graph(model, torch.zeros(1, 3, 64, 64).to(device))
+writer.close()
