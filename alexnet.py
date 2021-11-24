aebe516ae122f408c053da2e4b4f70184d9de317
Jian Sun <sunjxan@gmail.com>
2021-11-24 10:00:44 +0800

    Update alexnet.py

--- alexnet.py
+++ alexnet.py
@@ -8,7 +8,7 @@ import os, time
 
 
 model_pkl = 'alexnet.pkl'
-parameters_pkl = 'alexnet-parameters.pkl'
+parameters_pkl = 'alexnet-parameters.pkl'  # PyTorch版本不同预训练权重地址可能不同  https://download.pytorch.org/models/alexnet-owt-7be5be79.pth
 optimizer_pkl = 'alexnet-optimizer.pkl'
 epochs = 200
 batch_size_train = 100
@@ -61,6 +61,8 @@ class AlexNet(nn.Module):
             nn.ReLU(True),
             nn.Linear(4096, num_classes),
         )
+        if init_weights:
+            self._initialize_weights()
 
     def forward(self, x):
         x = self.features(x)
@@ -68,6 +70,20 @@ class AlexNet(nn.Module):
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
+
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
 
 # 可视化 tensorboard --logdir=runs-alexnet --bind_all
@@ -83,12 +99,11 @@ else:
 # 程序中会对可见GPU重新从0编号
 device = torch.device("cuda:0" if cuda_available else "cpu")
 # 模型
-model = AlexNet()
 if os.path.isfile(parameters_pkl):
+    model = AlexNet(init_weights=False)
     model.load_state_dict(torch.load(parameters_pkl))
 else:
-    # PyTorch版本不同预训练权重地址可能不同  https://download.pytorch.org/models/alexnet-owt-7be5be79.pth
-    model.load_state_dict(torch.load("/home/sunjian/.cache/torch/hub/checkpoints/alexnet-owt-7be5be79.pth"))
+    model = AlexNet()
 if cuda_available and device_count > 1:
     model = nn.DataParallel(model, device_ids=list(range(device_count)), output_device=0)
 model = model.to(device)
