a99012eeb07d4a81bc246bf1cffb251be3a55295
Jian Sun <sunjxan@gmail.com>
2021-12-08 17:49:19 +0800

    Update shufflenetv2.py

--- shufflenetv2.py
+++ shufflenetv2.py
@@ -1,50 +1,53 @@
 import torch
-from torch import Tensor
+import torchvision as tv
 import torch.nn as nn
-from .._internally_replaced_utils import load_state_dict_from_url
-from typing import Callable, Any, List
+import torch.optim as optim
+from tensorboardX import SummaryWriter
+
+import os, time
 
 
-__all__ = [
-    'ShuffleNetV2', 'shufflenet_v2_x0_5', 'shufflenet_v2_x1_0',
-    'shufflenet_v2_x1_5', 'shufflenet_v2_x2_0'
-]
+model_pkl = 'shufflenet_v2.pkl'
+parameters_pkl = 'shufflenet_v2-parameters.pkl'
+optimizer_pkl = 'shufflenet_v2-optimizer.pkl'
+epochs = 200
+batch_size_train = 100
+batch_size_val = 1000
+learning_rate = 1e-3
+log_interval_steps = 250
+# 对于可重复的实验，设置随机种子
+torch.manual_seed(seed=1)
 
-model_urls = {
-    'shufflenetv2_x0.5': 'https://download.pytorch.org/models/shufflenetv2_x0.5-f707e7126e.pth',
-    'shufflenetv2_x1.0': 'https://download.pytorch.org/models/shufflenetv2_x1-5666bf0f80.pth',
-    'shufflenetv2_x1.5': None,
-    'shufflenetv2_x2.0': None,
-}
+
+# 转换器，将PIL Image转换为Tensor
+transform = tv.transforms.Compose([tv.transforms.ToTensor()])
+# 训练集（有标签），(100000, 2, 3, 64, 64)
+train_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/train', transform=transform)
+# 验证集（有标签），(10000, 2, 3, 64, 64)
+val_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/val', transform=transform)
+# 测试集（无标签），(10000, 2, 3, 64, 64)
+test_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/test', transform=transform)
+# 训练数据生成器，先随机打乱，然后按batch_size分批，样本不重不漏，最后一个batch样本数量可能不足batch_size
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_train, shuffle=True)
+# 测试数据生成器
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size_val, shuffle=False)
 
 
-def channel_shuffle(x: Tensor, groups: int) -> Tensor:
+def channel_shuffle(x, groups):
     batchsize, num_channels, height, width = x.size()
     channels_per_group = num_channels // groups
 
-    # reshape
-    x = x.view(batchsize, groups,
-               channels_per_group, height, width)
-
+    x = x.view(batchsize, groups, channels_per_group, height, width)
     x = torch.transpose(x, 1, 2).contiguous()
-
-    # flatten
     x = x.view(batchsize, -1, height, width)
-
     return x
 
 
 class InvertedResidual(nn.Module):
-    def __init__(
-        self,
-        inp: int,
-        oup: int,
-        stride: int
-    ) -> None:
-        super(InvertedResidual, self).__init__()
+    def __init__(self, inp, oup, stride):
+        super().__init__()
 
-        if not (1 <= stride <= 3):
-            raise ValueError('illegal stride value')
+        assert stride in [1, 2, 3]
         self.stride = stride
 
         branch_features = oup // 2
@@ -62,8 +65,7 @@ class InvertedResidual(nn.Module):
             self.branch1 = nn.Sequential()
 
         self.branch2 = nn.Sequential(
-            nn.Conv2d(inp if (self.stride > 1) else branch_features,
-                      branch_features, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.Conv2d(inp if (self.stride > 1) else branch_features, branch_features, kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(branch_features),
             nn.ReLU(inplace=True),
             self.depthwise_conv(branch_features, branch_features, kernel_size=3, stride=self.stride, padding=1),
@@ -74,17 +76,10 @@ class InvertedResidual(nn.Module):
         )
 
     @staticmethod
-    def depthwise_conv(
-        i: int,
-        o: int,
-        kernel_size: int,
-        stride: int = 1,
-        padding: int = 0,
-        bias: bool = False
-    ) -> nn.Conv2d:
+    def depthwise_conv(i, o, kernel_size, stride=1, padding=0, bias=False):
         return nn.Conv2d(i, o, kernel_size, stride, padding, bias=bias, groups=i)
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x):
         if self.stride == 1:
             x1, x2 = x.chunk(2, dim=1)
             out = torch.cat((x1, self.branch2(x2)), dim=1)
@@ -92,24 +87,13 @@ class InvertedResidual(nn.Module):
             out = torch.cat((self.branch1(x), self.branch2(x)), dim=1)
 
         out = channel_shuffle(out, 2)
-
         return out
 
-
 class ShuffleNetV2(nn.Module):
-    def __init__(
-        self,
-        stages_repeats: List[int],
-        stages_out_channels: List[int],
-        num_classes: int = 1000,
-        inverted_residual: Callable[..., nn.Module] = InvertedResidual
-    ) -> None:
-        super(ShuffleNetV2, self).__init__()
+    def __init__(self, stages_repeats, stages_out_channels, num_classes=1000, inverted_residual=InvertedResidual, init_weights=True):
+        super().__init__()
 
-        if len(stages_repeats) != 3:
-            raise ValueError('expected stages_repeats as list of 3 positive ints')
-        if len(stages_out_channels) != 5:
-            raise ValueError('expected stages_out_channels as list of 5 positive ints')
+        assert len(stages_repeats) == 3 and len(stages_out_channels) == 5
         self._stage_out_channels = stages_out_channels
 
         input_channels = 3
@@ -123,13 +107,8 @@ class ShuffleNetV2(nn.Module):
 
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        # Static annotations for mypy
-        self.stage2: nn.Sequential
-        self.stage3: nn.Sequential
-        self.stage4: nn.Sequential
         stage_names = ['stage{}'.format(i) for i in [2, 3, 4]]
-        for name, repeats, output_channels in zip(
-                stage_names, stages_repeats, self._stage_out_channels[1:]):
+        for name, repeats, output_channels in zip(stage_names, stages_repeats, self._stage_out_channels[1:]):
             seq = [inverted_residual(input_channels, output_channels, 2)]
             for i in range(repeats - 1):
                 seq.append(inverted_residual(output_channels, output_channels, 1))
@@ -145,87 +124,197 @@ class ShuffleNetV2(nn.Module):
 
         self.fc = nn.Linear(output_channels, num_classes)
 
-    def _forward_impl(self, x: Tensor) -> Tensor:
-        # See note [TorchScript super()]
+        if init_weights:
+            self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
         x = self.conv1(x)
         x = self.maxpool(x)
         x = self.stage2(x)
         x = self.stage3(x)
         x = self.stage4(x)
         x = self.conv5(x)
-        x = x.mean([2, 3])  # globalpool
+        x = x.mean([2, 3])
         x = self.fc(x)
         return x
 
-    def forward(self, x: Tensor) -> Tensor:
-        return self._forward_impl(x)
+# PyTorch版本不同预训练权重地址可能不同
+model_urls = {
+    'shufflenetv2_x0.5': 'https://download.pytorch.org/models/shufflenetv2_x0.5-f707e7126e.pth',
+    'shufflenetv2_x1.0': 'https://download.pytorch.org/models/shufflenetv2_x1-5666bf0f80.pth',
+    'shufflenetv2_x1.5': None,
+    'shufflenetv2_x2.0': None,
+}
+
+# shufflenet_v2_x0_5 = ShuffleNetV2([4, 8, 4], [24, 48, 96, 192, 1024])
+# shufflenet_v2_x1_0 = ShuffleNetV2([4, 8, 4], [24, 116, 232, 464, 1024])
+# shufflenet_v2_x1_5 = ShuffleNetV2([4, 8, 4], [24, 176, 352, 704, 1024])
+# shufflenet_v2_x2_0 = ShuffleNetV2([4, 8, 4], [24, 244, 488, 976, 2048])
 
 
-def _shufflenetv2(arch: str, pretrained: bool, progress: bool, *args: Any, **kwargs: Any) -> ShuffleNetV2:
-    model = ShuffleNetV2(*args, **kwargs)
-
-    if pretrained:
-        model_url = model_urls[arch]
-        if model_url is None:
-            raise NotImplementedError('pretrained {} is not supported as of now'.format(arch))
-        else:
-            state_dict = load_state_dict_from_url(model_url, progress=progress)
-            model.load_state_dict(state_dict)
-
-    return model
-
-
-def shufflenet_v2_x0_5(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ShuffleNetV2:
-    """
-    Constructs a ShuffleNetV2 with 0.5x output channels, as described in
-    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
-    <https://arxiv.org/abs/1807.11164>`_.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _shufflenetv2('shufflenetv2_x0.5', pretrained, progress,
-                         [4, 8, 4], [24, 48, 96, 192, 1024], **kwargs)
-
-
-def shufflenet_v2_x1_0(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ShuffleNetV2:
-    """
-    Constructs a ShuffleNetV2 with 1.0x output channels, as described in
-    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
-    <https://arxiv.org/abs/1807.11164>`_.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _shufflenetv2('shufflenetv2_x1.0', pretrained, progress,
-                         [4, 8, 4], [24, 116, 232, 464, 1024], **kwargs)
+# 可视化 tensorboard --logdir=runs-shufflenet_v2 --bind_all
+writer = SummaryWriter(logdir='runs-shufflenet_v2')
+# 设备
+# 执行前设置环境变量 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" python3 filename.py
+# 程序中会对可见GPU重新从0编号
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+if device.type == 'cuda':
+    device_count = torch.cuda.device_count()
+    print('use {} gpu(s)'.format(device_count))
+else:
+    print('use cpu')
+# 模型
+if os.path.isfile(parameters_pkl):
+    model = ShuffleNetV2([4, 8, 4], [24, 48, 96, 192, 1024], init_weights=False)
+    model.load_state_dict(torch.load(parameters_pkl))
+else:
+    model = ShuffleNetV2([4, 8, 4], [24, 48, 96, 192, 1024])
+if device.type == 'cuda' and device_count > 1:
+    model = nn.DataParallel(model, device_ids=list(range(device_count)), output_device=0)
+model = model.to(device)
+# 损失函数
+criterion = nn.CrossEntropyLoss()
+# 优化器
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+if os.path.isfile(optimizer_pkl):
+    optimizer.load_state_dict(torch.load(optimizer_pkl))
 
 
-def shufflenet_v2_x1_5(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ShuffleNetV2:
-    """
-    Constructs a ShuffleNetV2 with 1.5x output channels, as described in
-    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
-    <https://arxiv.org/abs/1807.11164>`_.
+def fit(model, optimizer, epochs, initial_epoch=1, baseline=True):
+    global global_step
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _shufflenetv2('shufflenetv2_x1.5', pretrained, progress,
-                         [4, 8, 4], [24, 176, 352, 704, 1024], **kwargs)
+    # 设置model.training为True
+    model.train()
+
+    steps_per_epoch = len(train_loader)
+    total_train_images = len(train_set)
+
+    for epoch_index in range(initial_epoch, initial_epoch + epochs):
+        print('Train Epoch {}/{}'.format(epoch_index, initial_epoch + epochs - 1))
+        print('-' * 20)
+
+        epoch_loss_sum = 0
+        epoch_correct_images = 0
+        epoch_begin = time.time()
+
+        for step_index, (images, labels) in enumerate(train_loader, 1):
+            step_input_images = images.shape[0]
+
+            step_begin = time.time()
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            step_end = time.time()
+            step_period = round((step_end - step_begin) * 1e3)
+
+            global_step += 1
+            step_loss = loss.item()
+            epoch_loss_sum += step_loss * step_input_images
+            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
+            epoch_correct_images += step_correct_images
+
+            if step_index % log_interval_steps == 0:
+                torch.save(model.state_dict(), parameters_pkl)
+                torch.save(optimizer.state_dict(), optimizer_pkl)
+
+                writer.add_scalar('train/loss', step_loss, global_step)
+                writer.add_scalar('train/accuracy', step_correct_images / step_input_images, global_step)
+
+                print('Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(step_index, steps_per_epoch, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
+
+        epoch_end = time.time()
+        epoch_period = round((epoch_end - epoch_begin) * 1e3)
+
+        print('-' * 20)
+        print('Train Epoch {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index, initial_epoch + epochs - 1, int(epoch_period / 1e3), epoch_period % 1e3, epoch_loss_sum / total_train_images, epoch_correct_images, total_train_images, 1e2 * epoch_correct_images / total_train_images))
+        print()
+
+    if baseline:
+        steps_total_val = len(val_loader)
+        baseline_loss = epoch_loss_sum / total_train_images
+        baseline_accuracy = epoch_correct_images / total_train_images
+        for i in range(1, steps_total_val + 1):
+            writer.add_scalars('test/loss', {'baseline': baseline_loss}, i)
+            writer.add_scalars('test/accuracy', {'baseline': baseline_accuracy}, i)
+
+global_step = 0
+fit(model, optimizer, epochs)
 
 
-def shufflenet_v2_x2_0(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ShuffleNetV2:
-    """
-    Constructs a ShuffleNetV2 with 2.0x output channels, as described in
-    `"ShuffleNet V2: Practical Guidelines for Efficient CNN Architecture Design"
-    <https://arxiv.org/abs/1807.11164>`_.
+def evaluate(model):
+    global global_step
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _shufflenetv2('shufflenetv2_x2.0', pretrained, progress,
-                         [4, 8, 4], [24, 244, 488, 976, 2048], **kwargs)
+    # 设置model.training为False
+    model.eval()
+
+    steps_total = len(val_loader)
+    total_loss_sum = 0
+    total_correct_images = 0
+    total_input_images = 0
+
+    with torch.no_grad():
+        print('Eval')
+        print('-' * 20)
+
+        val_begin = time.time()
+
+        for step_index, (images, labels) in enumerate(val_loader, 1):
+            step_input_images = images.shape[0]
+            total_input_images += step_input_images
+
+            step_begin = time.time()
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            step_end = time.time()
+            step_period = round((step_end - step_begin) * 1e3)
+
+            global_step += 1
+            step_loss = loss.item()
+            total_loss_sum += step_loss * step_input_images
+            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
+            total_correct_images += step_correct_images
+
+            writer.add_scalars('validate/loss', {'current': step_loss, 'average': total_loss_sum / total_input_images}, global_step)
+            writer.add_scalars('validate/accuracy', {'current': step_correct_images / step_input_images, 'average': total_correct_images / total_input_images}, global_step)
+
+            print('Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(step_index, steps_total, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
+
+        val_end = time.time()
+        val_period = round((val_end - val_begin) * 1e3)
+
+        print('-' * 20)
+        print('Eval  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(int(val_period / 1e3), val_period % 1e3, total_loss_sum / total_input_images, total_correct_images, total_input_images, 1e2 * total_correct_images / total_input_images))
+        print()
+
+global_step = 0
+evaluate(model)
+
+
+torch.save(model, model_pkl)
+writer.add_graph(model, torch.zeros(1, 3, 64, 64).to(device))
+writer.close()
