1422c8e5998de1e30ec21163ce1e9d6c91561a79
Jian Sun <sunjxan@gmail.com>
2021-12-08 17:25:48 +0800

    Update squeezenet.py

--- squeezenet.py
+++ squeezenet.py
@@ -1,38 +1,52 @@
 import torch
+import torchvision as tv
 import torch.nn as nn
-import torch.nn.init as init
-from .._internally_replaced_utils import load_state_dict_from_url
-from typing import Any
+import torch.optim as optim
+from tensorboardX import SummaryWriter
 
-__all__ = ['SqueezeNet', 'squeezenet1_0', 'squeezenet1_1']
+import os, time
 
-model_urls = {
-    'squeezenet1_0': 'https://download.pytorch.org/models/squeezenet1_0-b66bff10.pth',
-    'squeezenet1_1': 'https://download.pytorch.org/models/squeezenet1_1-b8a52dc0.pth',
-}
+
+model_pkl = 'squeezenet.pkl'
+parameters_pkl = 'squeezenet-parameters.pkl'
+optimizer_pkl = 'squeezenet-optimizer.pkl'
+epochs = 200
+batch_size_train = 100
+batch_size_val = 1000
+learning_rate = 1e-3
+log_interval_steps = 250
+# 对于可重复的实验，设置随机种子
+torch.manual_seed(seed=1)
+
+
+# 转换器，将PIL Image转换为Tensor
+transform = tv.transforms.Compose([tv.transforms.ToTensor()])
+# 训练集（有标签），(100000, 2, 3, 64, 64)
+train_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/train', transform=transform)
+# 验证集（有标签），(10000, 2, 3, 64, 64)
+val_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/val', transform=transform)
+# 测试集（无标签），(10000, 2, 3, 64, 64)
+test_set = tv.datasets.ImageFolder(root='./data/tiny-imagenet-200/test', transform=transform)
+# 训练数据生成器，先随机打乱，然后按batch_size分批，样本不重不漏，最后一个batch样本数量可能不足batch_size
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_train, shuffle=True)
+# 测试数据生成器
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size_val, shuffle=False)
 
 
 class Fire(nn.Module):
 
-    def __init__(
-        self,
-        inplanes: int,
-        squeeze_planes: int,
-        expand1x1_planes: int,
-        expand3x3_planes: int
-    ) -> None:
-        super(Fire, self).__init__()
+    def __init__(self, inplanes, squeeze_planes, expand1x1_planes, expand3x3_planes):
+        super().__init__()
+
         self.inplanes = inplanes
         self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
         self.squeeze_activation = nn.ReLU(inplace=True)
-        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
-                                   kernel_size=1)
+        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes, kernel_size=1)
         self.expand1x1_activation = nn.ReLU(inplace=True)
-        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
-                                   kernel_size=3, padding=1)
+        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes, kernel_size=3, padding=1)
         self.expand3x3_activation = nn.ReLU(inplace=True)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         x = self.squeeze_activation(self.squeeze(x))
         return torch.cat([
             self.expand1x1_activation(self.expand1x1(x)),
@@ -42,13 +56,11 @@ class Fire(nn.Module):
 
 class SqueezeNet(nn.Module):
 
-    def __init__(
-        self,
-        version: str = '1_0',
-        num_classes: int = 1000
-    ) -> None:
-        super(SqueezeNet, self).__init__()
+    def __init__(self, version='1_0', num_classes=1000, init_weights=True):
+        super().__init__()
+
         self.num_classes = num_classes
+
         if version == '1_0':
             self.features = nn.Sequential(
                 nn.Conv2d(3, 96, kernel_size=7, stride=2),
@@ -81,14 +93,7 @@ class SqueezeNet(nn.Module):
                 Fire(384, 64, 256, 256),
                 Fire(512, 64, 256, 256),
             )
-        else:
-            # FIXME: Is this needed? SqueezeNet should only be called from the
-            # FIXME: squeezenet1_x() functions
-            # FIXME: This checking is not done for the other models
-            raise ValueError("Unsupported SqueezeNet version {version}:"
-                             "1_0 or 1_1 expected".format(version=version))
 
-        # Final convolution is initialized differently from the rest
         final_conv = nn.Conv2d(512, self.num_classes, kernel_size=1)
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
@@ -97,53 +102,184 @@ class SqueezeNet(nn.Module):
             nn.AdaptiveAvgPool2d((1, 1))
         )
 
+        if init_weights:
+            self._initialize_weights(final_conv)
+
+    def _initialize_weights(self, final_conv):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 if m is final_conv:
-                    init.normal_(m.weight, mean=0.0, std=0.01)
+                    nn.init.normal_(m.weight, mean=0.0, std=0.01)
                 else:
-                    init.kaiming_uniform_(m.weight)
+                    nn.init.kaiming_uniform_(m.weight)
                 if m.bias is not None:
-                    init.constant_(m.bias, 0)
+                    nn.init.constant_(m.bias, 0)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         x = self.features(x)
         x = self.classifier(x)
         return torch.flatten(x, 1)
 
+# PyTorch版本不同预训练权重地址可能不同
+model_urls = {
+    'squeezenet1_0': 'https://download.pytorch.org/models/squeezenet1_0-b66bff10.pth',
+    'squeezenet1_1': 'https://download.pytorch.org/models/squeezenet1_1-b8a52dc0.pth',
+}
 
-def _squeezenet(version: str, pretrained: bool, progress: bool, **kwargs: Any) -> SqueezeNet:
-    model = SqueezeNet(version, **kwargs)
-    if pretrained:
-        arch = 'squeezenet' + version
-        state_dict = load_state_dict_from_url(model_urls[arch],
-                                              progress=progress)
-        model.load_state_dict(state_dict)
-    return model
-
-
-def squeezenet1_0(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> SqueezeNet:
-    r"""SqueezeNet model architecture from the `"SqueezeNet: AlexNet-level
-    accuracy with 50x fewer parameters and <0.5MB model size"
-    <https://arxiv.org/abs/1602.07360>`_ paper.
-    The required minimum input size of the model is 21x21.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _squeezenet('1_0', pretrained, progress, **kwargs)
+# squeezenet1_0 = SqueezeNet('1_0')
+# squeezenet1_1 = SqueezeNet('1_1')
 
 
-def squeezenet1_1(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> SqueezeNet:
-    r"""SqueezeNet 1.1 model from the `official SqueezeNet repo
-    <https://github.com/DeepScale/SqueezeNet/tree/master/SqueezeNet_v1.1>`_.
-    SqueezeNet 1.1 has 2.4x less computation and slightly fewer parameters
-    than SqueezeNet 1.0, without sacrificing accuracy.
-    The required minimum input size of the model is 17x17.
+# 可视化 tensorboard --logdir=runs-squeezenet --bind_all
+writer = SummaryWriter(logdir='runs-squeezenet')
+# 设备
+# 执行前设置环境变量 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" python3 filename.py
+# 程序中会对可见GPU重新从0编号
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+if device.type == 'cuda':
+    device_count = torch.cuda.device_count()
+    print('use {} gpu(s)'.format(device_count))
+else:
+    print('use cpu')
+# 模型
+if os.path.isfile(parameters_pkl):
+    model = SqueezeNet(init_weights=False)
+    model.load_state_dict(torch.load(parameters_pkl))
+else:
+    model = SqueezeNet()
+if device.type == 'cuda' and device_count > 1:
+    model = nn.DataParallel(model, device_ids=list(range(device_count)), output_device=0)
+model = model.to(device)
+# 损失函数
+criterion = nn.CrossEntropyLoss()
+# 优化器
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+if os.path.isfile(optimizer_pkl):
+    optimizer.load_state_dict(torch.load(optimizer_pkl))
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _squeezenet('1_1', pretrained, progress, **kwargs)
+
+def fit(model, optimizer, epochs, initial_epoch=1, baseline=True):
+    global global_step
+
+    # 设置model.training为True
+    model.train()
+
+    steps_per_epoch = len(train_loader)
+    total_train_images = len(train_set)
+
+    for epoch_index in range(initial_epoch, initial_epoch + epochs):
+        print('Train Epoch {}/{}'.format(epoch_index, initial_epoch + epochs - 1))
+        print('-' * 20)
+
+        epoch_loss_sum = 0
+        epoch_correct_images = 0
+        epoch_begin = time.time()
+
+        for step_index, (images, labels) in enumerate(train_loader, 1):
+            step_input_images = images.shape[0]
+
+            step_begin = time.time()
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            step_end = time.time()
+            step_period = round((step_end - step_begin) * 1e3)
+
+            global_step += 1
+            step_loss = loss.item()
+            epoch_loss_sum += step_loss * step_input_images
+            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
+            epoch_correct_images += step_correct_images
+
+            if step_index % log_interval_steps == 0:
+                torch.save(model.state_dict(), parameters_pkl)
+                torch.save(optimizer.state_dict(), optimizer_pkl)
+
+                writer.add_scalar('train/loss', step_loss, global_step)
+                writer.add_scalar('train/accuracy', step_correct_images / step_input_images, global_step)
+
+                print('Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(step_index, steps_per_epoch, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
+
+        epoch_end = time.time()
+        epoch_period = round((epoch_end - epoch_begin) * 1e3)
+
+        print('-' * 20)
+        print('Train Epoch {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(epoch_index, initial_epoch + epochs - 1, int(epoch_period / 1e3), epoch_period % 1e3, epoch_loss_sum / total_train_images, epoch_correct_images, total_train_images, 1e2 * epoch_correct_images / total_train_images))
+        print()
+
+    if baseline:
+        steps_total_val = len(val_loader)
+        baseline_loss = epoch_loss_sum / total_train_images
+        baseline_accuracy = epoch_correct_images / total_train_images
+        for i in range(1, steps_total_val + 1):
+            writer.add_scalars('test/loss', {'baseline': baseline_loss}, i)
+            writer.add_scalars('test/accuracy', {'baseline': baseline_accuracy}, i)
+
+global_step = 0
+fit(model, optimizer, epochs)
+
+
+def evaluate(model):
+    global global_step
+
+    # 设置model.training为False
+    model.eval()
+
+    steps_total = len(val_loader)
+    total_loss_sum = 0
+    total_correct_images = 0
+    total_input_images = 0
+
+    with torch.no_grad():
+        print('Eval')
+        print('-' * 20)
+
+        val_begin = time.time()
+
+        for step_index, (images, labels) in enumerate(val_loader, 1):
+            step_input_images = images.shape[0]
+            total_input_images += step_input_images
+
+            step_begin = time.time()
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            step_end = time.time()
+            step_period = round((step_end - step_begin) * 1e3)
+
+            global_step += 1
+            step_loss = loss.item()
+            total_loss_sum += step_loss * step_input_images
+            step_correct_images = (torch.argmax(outputs, -1) == labels).sum().item()
+            total_correct_images += step_correct_images
+
+            writer.add_scalars('validate/loss', {'current': step_loss, 'average': total_loss_sum / total_input_images}, global_step)
+            writer.add_scalars('validate/accuracy', {'current': step_correct_images / step_input_images, 'average': total_correct_images / total_input_images}, global_step)
+
+            print('Step {}/{}  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(step_index, steps_total, int(step_period / 1e3), step_period % 1e3, step_loss, step_correct_images, step_input_images, 1e2 * step_correct_images / step_input_images))
+
+        val_end = time.time()
+        val_period = round((val_end - val_begin) * 1e3)
+
+        print('-' * 20)
+        print('Eval  Time: {:.0f}s {:.0f}ms  Loss: {:.4f}  Accuracy: {}/{} ({:.1f}%)'.format(int(val_period / 1e3), val_period % 1e3, total_loss_sum / total_input_images, total_correct_images, total_input_images, 1e2 * total_correct_images / total_input_images))
+        print()
+
+global_step = 0
+evaluate(model)
+
+
+torch.save(model, model_pkl)
+writer.add_graph(model, torch.zeros(1, 3, 64, 64).to(device))
+writer.close()
