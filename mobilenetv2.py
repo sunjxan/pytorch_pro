15ba7562bd70c74f1956e75e45f168cbd377f8bd
Jian Sun <sunjxan@gmail.com>
2021-12-08 17:01:24 +0800

    Update mobilenetv2.py

--- mobilenetv2.py
+++ mobilenetv2.py
@@ -4,9 +4,6 @@ import torch.nn as nn
 import torch.optim as optim
 from tensorboardX import SummaryWriter
 
-from torchvision.ops.misc import ConvNormActivation
-from torchvision._utils import _make_divisible
-
 import os, time
 
 
@@ -36,15 +33,30 @@ train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size_trai
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size_val, shuffle=False)
 
 
+def _make_divisible(v, divisor, min_value=None):
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_planes),
+            nn.ReLU6(inplace=True)
+        )
+
 class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, stride, expand_ratio, norm_layer=None):
+    def __init__(self, inp, oup, stride, expand_ratio):
         super().__init__()
 
         assert stride in [1, 2]
         self.stride = stride
-        
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
 
         hidden_dim = int(round(inp * expand_ratio))
         self.use_res_connect = self.stride == 1 and inp == oup
@@ -52,17 +64,15 @@ class InvertedResidual(nn.Module):
         layers = []
         if expand_ratio != 1:
             # pw
-            layers.append(ConvNormActivation(inp, hidden_dim, kernel_size=1, norm_layer=norm_layer, activation_layer=nn.ReLU6))
+            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1))
         layers.extend([
             # dw
-            ConvNormActivation(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim, norm_layer=norm_layer, activation_layer=nn.ReLU6),
+            ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim),
             # pw-linear
             nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-            norm_layer(oup),
+            nn.BatchNorm2d(oup),
         ])
         self.conv = nn.Sequential(*layers)
-        self.out_channels = oup
-        self._is_cn = stride > 1
 
     def forward(self, x):
         if self.use_res_connect:
@@ -71,14 +81,11 @@ class InvertedResidual(nn.Module):
             return self.conv(x)
 
 class MobileNetV2(nn.Module):
-    def __init__(self, num_classes=1000, width_mult=1.0, inverted_residual_setting=None, round_nearest=8, block=None, norm_layer=None):
+    def __init__(self, num_classes=1000, width_mult=1.0, inverted_residual_setting=None, round_nearest=8, block=None, init_weights=True):
         super().__init__()
 
         if block is None:
             block = InvertedResidual
-
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
 
         input_channel = 32
         last_channel = 1280
@@ -101,16 +108,16 @@ class MobileNetV2(nn.Module):
 
         input_channel = _make_divisible(input_channel * width_mult, round_nearest)
         self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
-        features = [ConvNormActivation(3, input_channel, stride=2, norm_layer=norm_layer, activation_layer=nn.ReLU6)]
+        features = [ConvBNReLU(3, input_channel, stride=2)]
 
         for t, c, n, s in inverted_residual_setting:
             output_channel = _make_divisible(c * width_mult, round_nearest)
             for i in range(n):
                 stride = s if i == 0 else 1
-                features.append(block(input_channel, output_channel, stride, expand_ratio=t, norm_layer=norm_layer))
+                features.append(block(input_channel, output_channel, stride, expand_ratio=t))
                 input_channel = output_channel
 
-        features.append(ConvNormActivation(input_channel, self.last_channel, kernel_size=1, norm_layer=norm_layer, activation_layer=nn.ReLU6))
+        features.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1))
 
         self.features = nn.Sequential(*features)
 
@@ -118,8 +125,9 @@ class MobileNetV2(nn.Module):
             nn.Dropout(0.2),
             nn.Linear(self.last_channel, num_classes),
         )
-
-        self._initialize_weights()
+        
+        if init_weights:
+            self._initialize_weights()
 
     def _initialize_weights(self):
         for m in self.modules():
